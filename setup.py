e7b8d68f280772ca814da859c34aced12e7060fd
NEVER FUCKING MORE <118989580+BEWARETHEDEAD@users.noreply.github.com>
2023-09-10 03:51:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ATools",
     version="0.1",
-    packages=find_packages(),
+    packages=["ATools"],
     install_requires=[
         'tontools',
         'xjet',
