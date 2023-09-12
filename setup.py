99a747aeb4f3908b93d1a58ce876042286927042
NEVER FUCKING MORE <118989580+BEWARETHEDEAD@users.noreply.github.com>
2023-09-12 15:18:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ATTools",
-    version="0.1.6",
+    version="0.1.7",
     packages=["ATTools"],
     install_requires=[
         'asyncio',
