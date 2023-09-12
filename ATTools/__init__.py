47034e9915b99a9b3482f69a846e0004e6ff4bbb
NEVER FUCKING MORE <118989580+BEWARETHEDEAD@users.noreply.github.com>
2023-09-12 07:07:55 +0300

    Update __init__.py

--- ATTools/__init__.py
+++ ATTools/__init__.py
@@ -1,3 +1,3 @@
 from .ATTools import *
 
-__all__ = [name for name in dir(ATools) if (isinstance(getattr(ATools, name), type) or callable(getattr(ATools, name))) and not name.startswith("__")]
+__all__ = [name for name in dir(ATTools) if (isinstance(getattr(ATTools, name), type) or callable(getattr(ATTools, name))) and not name.startswith("__")]
